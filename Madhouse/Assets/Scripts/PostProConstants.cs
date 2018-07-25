e899b36cf2c802b02116faf038c7cf7fe8698fbd
Monika <monika.galkewitsch@hs-furtwangen.de>
2018-07-25 12:35:22 +0200

    ColorGrading and Depth of Field Effects

---
+++ Madhouse/Assets/Scripts/PostProConstants.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.Rendering.PostProcessing;
+
+static class PostProConstants
+{
+    //Depth of Fieled
+    public const float focusDistance = 1f;
+    public const float aperature = 5.5f;
+    public const float normalFocusLength = 35f;
+    public const float insaneFocusLength = 50f;
+
+    //Color Grading
+    public const GradingMode gradingMode = GradingMode.LowDefinitionRange;
+    public const float temperature = 40f;
+    public const float saturation = 75f;
+    public const float contrast = 10f;
+
+    //Bloom
+}
