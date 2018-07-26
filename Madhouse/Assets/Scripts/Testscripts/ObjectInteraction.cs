abbef648eebbff78780eb9b85fd6f74fedc2f416
Monika <monika.galkewitsch@hs-furtwangen.de>
2018-07-26 10:10:19 +0200

    Small Changes to ObjectInteraction
    oh and a camera Setup Prefab

--- Madhouse/Assets/Scripts/Testscripts/ObjectInteraction.cs
+++ Madhouse/Assets/Scripts/Testscripts/ObjectInteraction.cs
@@ -8,14 +8,13 @@ public class ObjectInteraction : MonoBehaviour {
 	void Update () {
 		if (Input.GetMouseButtonDown(0)){
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-            GameObject target = hit.transform.gameObject;
-
+            RaycastHit hit;
             if (Physics.Raycast(ray, out hit)){
-				if((this.transform.position - hit.transform.position).magnitude < Config.interactionDistance){
-					if(hit.transform.gameObject.GetComponent<Door>() != null){
+                GameObject target = hit.transform.gameObject;
+                if ((this.transform.position - hit.transform.position).magnitude < Config.interactionDistance){
+					if(target.GetComponent<Door>() != null){
 						Debug.Log("Door");
-					} else if (hit.transform.gameObject.GetComponent<Container>() != null){
+					} else if (target.GetComponent<Container>() != null){
 						ItemDragHandler idh = Camera.main.GetComponent<ItemDragHandler>();
 						idh.enabled = true;
 						idh.addContainer(hit.transform.gameObject.GetComponent<Container>());
